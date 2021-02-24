f2361faccc4290f50220959d7bde3ab6d49b47a1
Lucas Löffel <lucas@loeffel.io>
2021-02-24 16:19:08 +0100

    init

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/makeless/makeless-go-event-client
 
 go 1.15
+
+require (
+	github.com/fatih/color v1.10.0
+	github.com/makeless/makeless-go v1.0.0-beta.26
+	github.com/r3labs/sse/v2 v2.3.0
+	golang.org/x/net v0.0.0-20210224082022-3d97a244fca7 // indirect
+	golang.org/x/sys v0.0.0-20210223212115-eede4237b368 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,49 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/fatih/color"
+	makeless_go_event_basic "github.com/makeless/makeless-go/event/basic"
+	"github.com/makeless/makeless-go/logger/basic"
+	"github.com/r3labs/sse/v2"
+	"sync"
+	"time"
+)
 
+func main() {
+	var colorGreen = color.New(color.FgGreen, color.Bold)
+	var colorWhite = color.New(color.FgWhite)
+	var colorHiWhite = color.New(color.FgHiWhite)
+
+	var logger = new(makeless_go_logger_basic.Logger)
+	var client = sse.NewClient("https://localhost:3003/api/auth/company/1/group/1/message/event")
+
+	if err := client.SubscribeRaw(func(event *sse.Event) {
+		if event == nil {
+			return
+		}
+
+		var data = &makeless_go_event_basic.EventData{
+			RWMutex: new(sync.RWMutex),
+		}
+
+		if err := json.Unmarshal(event.Data, data); err != nil {
+			logger.Fatal(fmt.Errorf("event not unmarshallable"))
+		}
+
+		if _, err := colorGreen.Printf("%s:%s\t\t", string(event.Event), data.GetId()); err != nil {
+			logger.Fatal(err)
+		}
+
+		if _, err := colorHiWhite.Printf("%s\t\t", data.GetData()); err != nil {
+			logger.Fatal(err)
+		}
+
+		if _, err := colorWhite.Printf("%s\n", time.Now().UTC().String()); err != nil {
+			logger.Fatal(err)
+		}
+	}); err != nil {
+		logger.Fatal(err)
+	}
 }
